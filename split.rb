26791a998dc43796ac0af1a33dc639acd940f649
Rafael Lima <rafa.cbl@gmail.com>
2018-05-15 14:43:56 +0200

    Add spec folder to parallel_rspec command

--- split.rb
+++ split.rb
@@ -1,3 +1,3 @@
 #exit_code_1 = system("TEST_ITERATION=1 DISABLE_SPRING=true RAILS_ENV=test bundle exec parallel_rspec -n 2 --runtime-log ./runtime_rspec.opts #{$files} -e 'ruby -e \"puts %[hello from process #{ENV[:TEST_ENV_NUMBER.to_s].inspect}]\"'")
 #puts exit_code_1
-system("bundle exec parallel_rspec -n 2")
+system("bundle exec parallel_rspec spec/ -n 2")
