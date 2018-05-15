39174f66189ef2182072605614d1a5f95ed06c6d
Rafael Lima <rafa.cbl@gmail.com>
2018-05-15 14:42:57 +0200

    Run parallel_rspec command

--- split.rb
+++ split.rb
@@ -1,2 +1,3 @@
-exit_code_1 = system("TEST_ITERATION=1 DISABLE_SPRING=true RAILS_ENV=test bundle exec parallel_rspec -n 2 --runtime-log ./runtime_rspec.opts #{$files} -e 'ruby -e \"puts %[hello from process #{ENV[:TEST_ENV_NUMBER.to_s].inspect}]\"'")
-puts exit_code_1
+#exit_code_1 = system("TEST_ITERATION=1 DISABLE_SPRING=true RAILS_ENV=test bundle exec parallel_rspec -n 2 --runtime-log ./runtime_rspec.opts #{$files} -e 'ruby -e \"puts %[hello from process #{ENV[:TEST_ENV_NUMBER.to_s].inspect}]\"'")
+#puts exit_code_1
+system("bundle exec parallel_rspec -n 2")
